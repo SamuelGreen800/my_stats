5c860781ed9473da8d327e32db3767c98d94900c
Samuel Green <samuelgreen800@tutanota.com>
2022-03-04 23:48:56 -0500

    1st java

---
+++ Stats.java
@@ -0,0 +1,7 @@
+class Stats{
+    public static void main(String args[]){
+        System.out.println("My name is Samuel Green");
+        System.out.println("I am 34 years old");
+        System.out.println("My hometown is the world");
+    }
+}
